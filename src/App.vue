31d62b17c4f596b0755eee9113e658fc55b5c5a9
alexRw21 <alexladriere@live.be>
2022-06-16 16:54:21 +0200

    workshop

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Header from "./components/Header.vue";
+</script>
+
+<template>
+  <div class="bg-slate-50 flex flex-col min-h-screen">
+    <Header />
+    <main class="container mx-auto m-8 border-2 border-dashed rounded p-8">
+      <RouterView />
+    </main>
+  </div>
+</template>
